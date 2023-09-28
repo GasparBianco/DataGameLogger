78cc140a2ddc0a040832ae6334d701e357af775f
Gaspar Bianco <gasparbianco98@gmail.com>
2023-09-28 16:04:48 -0300

    gaspar/FEATURE-auth-with-jwt

--- DataGameLogger/routers/userRouter.py
+++ DataGameLogger/routers/userRouter.py
@@ -4,31 +4,16 @@ from schemas.userSchema import *
 from config.db_config import *
 from sqlalchemy.orm import Session
 from validations.usuerValidations import userRegisterValidations
-
+from .auth import current_user
 
 
 router = APIRouter(prefix="/user",
                     tags=["user"],
                     responses={404: {"message": "No encontrado"}})
 
-@router.post("/register/", response_model= UserResponse, status_code=status.HTTP_201_CREATED)
-async def registerUser(user_data: UserRegister, db: Session = Depends(get_db)):
-    
-    userRegisterValidations(user_data, db)
-
-    new_user = User(
-                    username = user_data.username,
-                    email = user_data.email,
-                    password = user_data.password
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-@router.get("/collection/{id}", response_model=UserCollection, status_code=status.HTTP_200_OK)
-async def getIngredientPage(id: int, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == id).first()
+@router.get("/collection/", response_model=UserCollection, status_code=status.HTTP_200_OK)
+async def getIngredientPage(user: User = Depends(current_user), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user.id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
