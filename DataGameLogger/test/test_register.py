7ec1e9b161b95c416553c97de07761280251eabf
Gaspar Bianco <gasparbianco98@gmail.com>
2023-10-20 22:10:24 -0300

    gaspar/TEST-register-test

--- DataGameLogger/test/test_register.py
+++ DataGameLogger/test/test_register.py
@@ -12,4 +12,49 @@ def test_register_new_user_succed():
     assert data["email"] == "test@test.com"
     assert data["username"] == "Tester"
     assert "id" in data
+    assert 'password' not in data
     
+def test_register_already_existent_username():
+    response = client.post(
+        "/auth/register",
+        json={  "email": "test2@test.com", 
+                "password": "Test.1234",
+                'username': 'Tester'},
+    )
+    assert response.status_code == 400, response.text
+
+def test_register_already_existent_email():
+    response = client.post(
+        "/auth/register",
+        json={  "email": "test@test.com", 
+                "password": "Test.1234",
+                'username': 'Tester2'},
+    )
+    assert response.status_code == 400, response.text
+
+def test_register_invalid_email():
+    response = client.post(
+        "/auth/register",
+        json={  "email": "test2testcom", 
+                "password": "Test.1234",
+                'username': 'Tester'},
+    )
+    assert response.status_code == 422, response.text
+
+def test_register_invalid_username():
+    response = client.post(
+        "/auth/register",
+        json={  "email": "test3@test.com", 
+                "password": "Test.1234",
+                'username': 'Te'},
+    )
+    assert response.status_code == 422, response.text
+
+def test_register_invalid_password():
+    response = client.post(
+        "/auth/register",
+        json={  "email": "test3@test.com", 
+                "password": "Te",
+                'username': 'Tester3'},
+    )
+    assert response.status_code == 422, response.text
